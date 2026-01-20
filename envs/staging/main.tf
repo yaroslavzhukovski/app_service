ff5d348d57fe00ae34d9a6f0015a21a19bd4da50
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-01-20 14:56:56 +0100

    App service plan and webapp

--- envs/staging/main.tf
+++ envs/staging/main.tf
@@ -15,3 +15,38 @@ module "networking" {
   subnets       = var.subnets
   tags          = var.tags
 }
+module "app_service_plan" {
+  source = "../../modules/app_service_plan"
+
+  name                = var.asp_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  sku_name = var.asp_sku_name
+  tags     = var.tags
+}
+
+module "app_service" {
+  source = "../../modules/app_service"
+
+  name                = var.webapp_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  service_plan_id = module.app_service_plan.id
+
+  app_settings = {
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+  }
+
+  web_app_slots = {
+    staging = {
+      name = "staging"
+      app_settings = {
+        ENVIRONMENT = "staging"
+      }
+    }
+  }
+
+  tags = var.tags
+}
