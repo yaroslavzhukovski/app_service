c9fe748d1a20c8c8d758047de367631aefe89e36
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-01-20 13:31:08 +0100

    Networking

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,15 +1,15 @@
 module "vnet" {
-  source  = "Azure/avm-res-network-virtualnetwork/azurerm/"
-  version = "0.17.1"
-  name=var.name
-  location=var.location
-  parent_id=var.resource_group_id
-    address_spaces=var.address_spaces
-    subnets{
-        for k, s in var.subnets : k => {
-            name=s.name
-            address_prefixes=s.address_prefixes
+  source        = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version       = "0.17.1"
+  name          = var.name
+  location      = var.location
+  parent_id     = var.resource_group_id
+  address_space = var.address_spaces
+  subnets = {
+    for k, s in var.subnets : k => {
+      name             = s.name
+      address_prefixes = s.address_prefixes
     }
-    }
-    tags=var.tags
+  }
+  tags = var.tags
 }
